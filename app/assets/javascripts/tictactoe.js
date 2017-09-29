acc010e7550340466d7fecb4b4797550e1bdb5e9
github username <github email address>
2017-09-29 12:46:52 -0400

    TTT game is working

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,3 +1,9 @@
+$(document).ready(function(){
+  attachListeners()
+})
+
+
+
 const win_combinations=
 [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
@@ -18,7 +24,13 @@ var player = function(){
 }
 
 var updateState = function(position){
-  $(position).html(player())
+   if($(position).text() == ""){
+        $(position).html(player())
+        turn++
+   }
+  else{
+      console.log("position taken!")
+    }
 }
 
 function setMessage(string){
@@ -43,7 +55,6 @@ function checkWinner(){
 
 function doTurn(position){
   updateState(position)
-  turn++
   if(checkWinner()){
   resetBoard()
   }
@@ -59,11 +70,16 @@ function resetBoard(){
 }
 
 function saveGame(){
-  
+
 }
 
 function attachListeners(){
-  $('tbody').click(function(event){
-    doTurn(event)
+
+  $('td').on('click', function(evt){
+    if (!checkWinner()){
+      doTurn(evt.target)
+      //if position !== " "
+        //insert "X" or "O"
+    }
   });
 }
